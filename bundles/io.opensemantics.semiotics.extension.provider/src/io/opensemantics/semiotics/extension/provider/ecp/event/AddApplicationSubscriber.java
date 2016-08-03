7601ad0fc96dac56b5f0f99f7d4f562a4a796f21
jonpasski <jon@passki.us>
2016-08-03 16:37:09 -0700

    38: Modify AddApplicationSubscriber to use Change Commands
    * Added the "EMF Model Transaction" feature to the target definition
    * Added to app dependency feature and as dependency to provider plugin
      * This requires `org.eclipse.emf.validation`
    * Added the "EMF Validation Framework." feature to target definition and
    app dependency feature.
    * Added the EMFStore 1.8 release site and "EMFStore Transactional
    Features"
    (`org.eclipse.emf.emfstore.client.transaction.feature`) to the target
    platform
    and app dependency feature. 
    * Ran local tests, all good.
    
    Task-Url: https://github.com/CoastalHacking/semiotics-main/issues/38

--- bundles/io.opensemantics.semiotics.extension.provider/src/io/opensemantics/semiotics/extension/provider/ecp/event/AddApplicationSubscriber.java
+++ bundles/io.opensemantics.semiotics.extension.provider/src/io/opensemantics/semiotics/extension/provider/ecp/event/AddApplicationSubscriber.java
@@ -15,10 +15,16 @@
  */
 package io.opensemantics.semiotics.extension.provider.ecp.event;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecp.core.ECPProject;
 import org.eclipse.emf.ecp.core.ECPProjectManager;
+import org.eclipse.emf.edit.command.ChangeCommand;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.event.Event;
@@ -27,6 +33,7 @@ import org.osgi.service.event.EventHandler;
 import org.osgi.service.log.LogService;
 
 import io.opensemantics.semiotics.extension.api.ModelFocal;
+import io.opensemantics.semiotics.extension.provider.ecp.TypedChangeCommand;
 import io.opensemantics.semiotics.model.assessment.Application;
 import io.opensemantics.semiotics.model.assessment.Applications;
 import io.opensemantics.semiotics.model.assessment.Assessment;
@@ -84,54 +91,90 @@ public class AddApplicationSubscriber implements EventHandler {
 
     final String iProjectName = iProject.getName();
     
+    // boiler-plate code start.
     if (projectManager == null) {
       String message = String.format("ProjectManager unbound; cannot add project %s", projectName);
       log(LogService.LOG_WARNING, message);
       return;
     }
     
-    ECPProject ecpProject = projectManager.getProject(projectName);
-    if (ecpProject != null && ecpProject.isOpen()) {
-      Assessment assessment = null;
-      for (Object content : ecpProject.getContents()) {
-        if (content instanceof Assessment) {
-          // FIXME: breaks on first match
-          assessment = (Assessment)content;
-          break;
-        }
-      }
+    final ECPProject ecpProject = projectManager.getProject(projectName);
+    if (ecpProject == null || !ecpProject.isOpen()) {
+      String message = String.format(
+          "No ECPProject with the name of %s or it is not open, not adding %s to it.",
+          projectName, iProjectName);
+      log(LogService.LOG_WARNING, message);
+      return;
+    }
 
-      if (assessment == null) {
-          assessment = AssessmentFactory.eINSTANCE.createAssessment();
-          ecpProject.getContents().add(assessment);
-      }
+    final EditingDomain editingDomain = ecpProject.getEditingDomain();
+    if (!(editingDomain instanceof TransactionalEditingDomain)) {
+      String message = String.format(
+          "The EditingDomain for ECPProject %s is not an instance of TransactionalEditingDomain",
+          projectName);
+      log(LogService.LOG_WARNING, message);
+      return;
+    }
+    // boiler-plate code end.
 
-      // Create or get applications container
-      Applications applications = assessment.getApplications();
-      if (applications == null) {
-        applications = AssessmentFactory.eINSTANCE.createApplications();
-        assessment.setApplications(applications);
-      }
-
-      EList<Application> apps = applications.getApplications();        
-      Application application = null;
-      for (Application app : apps) {
-        if (app.getLabel() != null && app.getLabel().equals(iProjectName)) {
-          application = app;
-          break;
-        }
-      }
-
-      if (application == null) {
-        application = AssessmentFactory.eINSTANCE.createApplication();
-        application.setLabel(iProjectName);
-        apps.add(application);
-        modelFocus.setFocus(application);
-      } else {
-        String message = String.format("Application already exists for project %s", iProjectName);
-        log(LogService.LOG_WARNING, message);
+    // Get the root EObject
+    Assessment assessment = null;
+    for (Object content : ecpProject.getContents()) {
+      if (content instanceof Assessment) {
+        // FIXME: breaks on first match
+        assessment = (Assessment)content;
+        break;
       }
     }
+
+    // Change commands
+    List<ChangeCommand> changeCommands = new ArrayList<>();
+    if (assessment == null) {
+      assessment = AssessmentFactory.eINSTANCE.createAssessment();
+      changeCommands.add(new TypedChangeCommand<Assessment>(assessment) {
+        @Override
+        protected void doExecute() {
+          ecpProject.getContents().add(root);
+        }
+      });
+    }
+
+    changeCommands.add(new TypedChangeCommand<Assessment>(assessment) {
+      @Override
+      protected void doExecute() {
+        // Create or get applications container
+        Applications applications = root.getApplications();
+        if (applications == null) {
+          applications = AssessmentFactory.eINSTANCE.createApplications();
+          root.setApplications(applications);
+        }
+
+        EList<Application> apps = applications.getApplications();        
+        Application application = null;
+        for (Application app : apps) {
+          if (app.getLabel() != null && app.getLabel().equals(iProjectName)) {
+            application = app;
+            break;
+          }
+        }
+
+        if (application == null) {
+          application = AssessmentFactory.eINSTANCE.createApplication();
+          application.setLabel(iProjectName);
+          apps.add(application);
+          modelFocus.setFocus(application);
+        } else {
+          String message = String.format("Application already exists for project %s", iProjectName);
+          log(LogService.LOG_WARNING, message);
+        }
+      }
+    });
+
+    // Apply changes
+    for (ChangeCommand command: changeCommands) {
+      editingDomain.getCommandStack().execute(command);
+    }
+
   }
 
   private void log(int severity, String message) {
